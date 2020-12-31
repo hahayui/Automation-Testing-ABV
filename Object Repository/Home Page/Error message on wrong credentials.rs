dac6ca4ba4ad2e273a6e18fc17c91ce2b725dabe
Elian Kurtev <eliankurtev@gmail.com>
2020-12-31 02:00:02 +0200

    Test Cases for the login, new objects and scripts.

---
+++ Object Repository/Home Page/Error message on wrong credentials.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Гревен потребител или парола</description>
+   <name>Error message on wrong credentials</name>
+   <tag></tag>
+   <elementGuidId>17e1e464-7025-459e-bbbf-8483bebacd95</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;form.errors&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>form.errors</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Home Page/Login password field.rs
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Login password field</description>
+   <name>Login password field</name>
+   <tag></tag>
+   <elementGuidId>bfc84c08-9276-4c2d-a523-4fd73c6c3f16</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'password']</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>password</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>password</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html/body/article/div/div[2]/form/div[2]/input</value>
+   </webElementProperties>
+</WebElementEntity>
